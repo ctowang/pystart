25dda77a2ee7597a0b8d87304c74b2576f367a74
ctowang <50625190@qq.com>
2019-04-29 11:46:54 +0800

    add file

--- service/mysql/mysql.py
+++ service/mysql/mysql.py
@@ -1,5 +1,24 @@
+# coding:utf-8
 import pymysql
-db = pymysql.connect(host='192.168.213.16',port = 28007,user='root',passwd='hz789123',db = 'api')
+import os,sys
+dirPath = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir,os.pardir))
+sys.path.append(dirPath)
+from configparser import ConfigParser
+
+#≥ı ºªØconfig¿‡
+cp = ConfigParser()
+cp.read("../../config/db.cfg")
+section = cp.sections()[0]
+
+sid = cp.get(section, "db")
+ip = cp.get(section, "host")
+name = cp.get(section, "user")
+pwd = cp.get(section, "passwd")
+pt = cp.getint(section, "port")
+
+
+
+db = pymysql.connect(host=ip,port=pt,user=name,passwd=pwd,db=sid)
 cursor = db.cursor()
 sql = "SELECT * FROM project"
 try:
